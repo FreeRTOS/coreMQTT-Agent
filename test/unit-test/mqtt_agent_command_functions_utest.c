ec78086f58b72ef8d38d1d5df8f1fd5167eae4b8
Ubuntu <ubuntu@ip-172-31-20-69.us-east-2.compute.internal>
2021-03-11 01:18:32 +0000

    Adressing Pr comments

---
+++ test/unit-test/mqtt_agent_command_functions_utest.c
@@ -0,0 +1,76 @@
+/*
+ * MQTTAgent v1.1.0
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * @file mqtt_agent_command_functions_utest.c
+ * @brief Unit tests for functions in mqtt_agent_utest.h
+ */
+#include <string.h>
+#include <stdbool.h>
+
+#include "unity.h"
+
+/* Include paths for public enums, structures, and macros. */
+
+#include "mock_core_mqtt.h"
+
+/**
+ * @brief MQTT client identifier.
+ */
+#define MQTT_CLIENT_IDENTIFIER                 "testclient"
+
+/**
+ * @brief A sample network context that we set to NULL.
+ */
+#define MQTT_SAMPLE_NETWORK_CONTEXT            ( NULL )
+
+/**
+ * @brief Time at the beginning of each test. Note that this is not updated with
+ * a real clock. Instead, we simply increment this variable.
+ */
+static uint32_t globalEntryTime = 0;
+
+
+/* ============================   UNITY FIXTURES ============================ */
+
+/* Called before each test method. */
+void setUp()
+{
+
+    globalEntryTime = 0;
+}
+
+/* Called after each test method. */
+void tearDown()
+{
+}
+
+/* Called at the beginning of the whole suite. */
+void suiteSetUp()
+{
+}
+
+/* Called at the end of the whole suite. */
+int suiteTearDown( int numFailures )
+{
+    return numFailures;
+}
