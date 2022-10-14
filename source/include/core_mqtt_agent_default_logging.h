86c51db1a7d060744a06fa3619bd3ba86a938a94
Aniruddha Kanhere <60444055+AniruddhaKanhere@users.noreply.github.com>
2022-10-14 09:27:11 -0700

    Update version number is source files and .md file for release (#78)
    * Update coreMQTT submodule pointer
    
    * Update core_mqtt_config.h
    
    * Update lexicon.txt
    
    * Update the version number for release

--- source/include/core_mqtt_agent_default_logging.h
+++ source/include/core_mqtt_agent_default_logging.h
@@ -1,5 +1,5 @@
 /*
- * coreMQTT-Agent v1.1.0
+ * coreMQTT-Agent v1.2.0
  * Copyright (C) 2022 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
